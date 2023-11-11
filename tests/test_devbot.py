4b485cb6bf80909e62870bdb7561085f8d273738
tolatolatop <tolatolatop@gmail.com>
2023-11-11 19:20:34 +0000

    防止接口未被测试

--- tests/test_devbot.py
+++ tests/test_devbot.py
@@ -25,3 +25,33 @@ def test_healthz(client):
     response = client.get("/healthz")
     assert response.status_code == 200
     assert response.json() == {"message": "OK"}
+
+
+def test_all_endpoints_have_tests():
+    from devbot import devbot
+
+    # 检查所有的测试方法是否以“test_”开头
+    test_methods = [
+        f"/{item.__name__}"
+        for item in globals().values()
+        if callable(item) and item.__name__.startswith("test_")
+    ]
+
+    endpoints = [
+        method.replace("test_", "")
+        for method in test_methods
+        if method != "test_all_endpoints_have_tests"
+    ]
+
+    endpoints.append("/") if "/root" in endpoints else None
+    endpoints.append("/openapi.json")
+    endpoints.append("/docs")
+    endpoints.append("/docs/oauth2-redirect")
+    endpoints.append("/redoc")
+
+    # 获取所有的接口路径和方法
+    routes = [route.path for route in devbot.app.routes if hasattr(route, "methods")]
+
+    # 断言每个接口路径是否出现在测试方法中
+    for route in routes:
+        assert route in endpoints
