4f1ca8d49291813b431a8f43a28d5572afd5fd15
tolatolatop <tolatolatop@gmail.com>
2023-11-12 20:41:50 +0000

    formatting

--- devbot/agent/core.py
+++ devbot/agent/core.py
@@ -7,39 +7,41 @@ from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 import tools
 import prompts
 
-from dotenv import load_dotenv
 
-load_dotenv()
+def create_agent_executor(repo_name: str, repo_url: str, commit_id: str):
+    root_path = tools.prepare_env(repo_url, repo_name, commit_id)
+    tools_list = tools.create_filesystem_tools(root_path) + []
+    use_prompt = prompts.coding_prompt
 
-repo_name = "tolatolatop/devbot"
-repo_url = f"https://github.com/{repo_name}"
-commit_id = "master"
-root_path = tools.prepare_env(repo_url, repo_name, commit_id)
-tools = tools.create_filesystem_tools(root_path) + []
-use_prompt = prompts.coding_prompt
+    llm = ChatOpenAI(temperature=0)
+    llm_with_tools = llm.bind(
+        functions=[format_tool_to_openai_function(t) for t in tools_list]
+    )
 
-llm = ChatOpenAI(temperature=0)
-llm_with_tools = llm.bind(
-    functions=[format_tool_to_openai_function(t) for t in tools]
-)
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_to_openai_functions(
+                x["intermediate_steps"]
+            ),
+            "chat_history": lambda x: x.get("chat_history") or [],
+        }
+        | use_prompt
+        | llm_with_tools
+        | OpenAIFunctionsAgentOutputParser()
+    ).with_config(run_name="Agent")
 
-
-agent = (
-    {
-        "input": lambda x: x["input"],
-        "agent_scratchpad": lambda x: format_to_openai_functions(
-            x["intermediate_steps"]
-        ),
-        "chat_history": lambda x: x.get("chat_history") or [],
-    }
-    | use_prompt
-    | llm_with_tools
-    | OpenAIFunctionsAgentOutputParser()
-).with_config(run_name="Agent")
+    agent_executor = AgentExecutor(agent=agent, tools=tools)
+    return agent_executor
 
 
-agent_executor = AgentExecutor(agent=agent, tools=tools)
+if __name__ == "__main__":
+    from dotenv import load_dotenv
 
-agent_executor.invoke(
-    {"input": "Summarize the results of the make test command"}
-)
+    load_dotenv()
+
+    repo_name = "tolatolatop/devbot"
+    repo_url = f"https://github.com/{repo_name}"
+    commit_id = "master"
+    ae = create_agent_executor(repo_url, repo_name, commit_id)
+    ae.invoke({"input": "Summarize the results of the make test command"})
