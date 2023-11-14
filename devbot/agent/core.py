ee212ed25b5e394b14cef7a353364afd151fe3f7
tolatolatop <tolatolatop@gmail.com>
2023-11-14 02:19:32 +0000

    解耦执行过程

--- devbot/agent/core.py
+++ devbot/agent/core.py
@@ -12,16 +12,17 @@ import tools
 import prompts
 
 
-def create_agent_executor(
-    repo_name: str, repo_url: str, commit_id: str, issue_number: int
-):
+def prepare_coding_agent(repo_name, repo_url, commit_id, issue_number):
     auth = Auth.Token(os.environ["GITHUB_TOKEN"])
     g = Github(auth=auth)
     root_path = tools.prepare_env(repo_url, repo_name, commit_id)
     chat_history = tools.create_issue_chat_history(g, repo_name, issue_number)
     tools_list = tools.create_filesystem_tools(root_path) + []
-    use_prompt = prompts.coding_prompt
+    prompt = prompts.coding_prompt
+    return prompt, tools_list, chat_history
 
+
+def create_agent_executor(use_prompt, tools_list, chat_history):
     llm = ChatOpenAI(temperature=0)
     llm_with_tools = llm.bind(
         functions=[format_tool_to_openai_function(t) for t in tools_list]
@@ -33,8 +34,7 @@ def create_agent_executor(
             "agent_scratchpad": lambda x: format_to_openai_functions(
                 x["intermediate_steps"]
             ),
-            "chat_history": lambda x: x.get("chat_history")
-            or chat_history[:-1],
+            "chat_history": lambda x: x.get("chat_history") or chat_history,
         }
         | use_prompt
         | llm_with_tools
@@ -42,8 +42,12 @@ def create_agent_executor(
     ).with_config(run_name="Agent")
 
     agent_executor = AgentExecutor(agent=agent, tools=tools_list)
-    agent_executor.invoke({"input": chat_history[-1].content})
     return agent_executor
+
+
+def run_agent_executor(input: str, agent_executor):
+    resp = agent_executor.invoke({"input": input})
+    return resp["output"]
 
 
 if __name__ == "__main__":
@@ -54,5 +58,10 @@ if __name__ == "__main__":
     repo_name = "tolatolatop/devbot"
     repo_url = f"https://github.com/{repo_name}"
     commit_id = "master"
-    issue_number = 13
-    ae = create_agent_executor(repo_name, repo_url, commit_id, issue_number)
+    issue_number = 15
+    use_prompt, tools_list, chat_history = prepare_coding_agent(
+        repo_name, repo_url, commit_id, issue_number
+    )
+    ae = create_agent_executor(use_prompt, tools_list, chat_history[:-1])
+    resp = run_agent_executor(chat_history[-1].content, ae)
+    print(resp)
