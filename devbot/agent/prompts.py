4379067a5389220125f2c14203167544274f7e25
tolatolatop <tolatolatop@gmail.com>
2023-11-14 19:36:12 +0000

    增加issue_prompt

--- devbot/agent/prompts.py
+++ devbot/agent/prompts.py
@@ -11,7 +11,7 @@ summary_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-coding_prompt = ChatPromptTemplate.from_messages(
+issue_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
@@ -19,6 +19,21 @@ coding_prompt = ChatPromptTemplate.from_messages(
             "Please answer the question based on the actual contents of repo."
             "You are allowed to use tools to view "
             "the repository and specific files",
+        ),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ]
+)
+
+coding_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a very good python engineer. Please complete the following actions according to user needs."
+            "Modify files according to the task to complete the task."
+            "Submit tasks to the warehouse through tools."
+            "If you have not submitted a PR before, please submit a PR through the tool.",
         ),
         MessagesPlaceholder(variable_name="chat_history"),
         ("user", "{input}"),
