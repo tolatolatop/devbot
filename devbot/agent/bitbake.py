d6d2222eb0ae860b0592de958afaeff185dbf052
tolatolatop <tolatolatop@gmail.com>
2023-12-08 17:39:45 +0000

    llm_chain

--- devbot/agent/bitbake.py
+++ devbot/agent/bitbake.py
@@ -12,6 +12,31 @@ from langchain.pydantic_v1 import BaseModel
 from langchain.callbacks.manager import CallbackManagerForToolRun
 from langchain.tools.file_management.list_dir import DirectoryListingInput
 from langchain.tools.file_management.read import ReadFileInput
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.globals import set_verbose
+
+from langchain.tools.render import render_text_description
+from langchain.chat_models import ChatAnthropic
+from langchain.memory.chat_message_histories import RedisChatMessageHistory
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import AgentAction, AgentFinish, StrOutputParser
+from langchain.schema.messages import AIMessage, HumanMessage, SystemMessage
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.schema.runnable import RunnableParallel
+from langchain.agents.agent_toolkits import FileManagementToolkit
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain.agents import AgentExecutor
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.schema import AgentAction, AgentFinish, OutputParserException
+
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+)
+from langchain.schema.output_parser import StrOutputParser
+
 
 from github import Github
 from github.Repository import Repository
@@ -98,3 +123,54 @@ class ReadFileTool(BaseGithubToolMixin, BaseTool):
             return file.decoded_content.decode("utf-8")
         except Exception as e:
             return "Error: " + str(e)
+
+
+class BitbakeAgentFactory:
+    def create_github_agent(
+        self, git_server: Github, repo: str, revision: str
+    ):
+        prompt = """
+Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+TIPS:
+{tips}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+Thought:{agent_scratchpad}
+    """
+        prompt = ChatPromptTemplate.from_template(prompt)
+        tools = GitHubToolkit(
+            github=git_server, repo="mirror/busybox", revision=revision
+        ).get_tools()
+
+        llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0)
+        llm = llm.bind(stop=["\nObservation"])
+        llm_chain = (
+            {
+                "tips": lambda x: "",
+                "input": lambda x: x["input"],
+                "agent_scratchpad": lambda x: format_log_to_str(
+                    x["intermediate_steps"]
+                ),
+                "tools": lambda x: render_text_description(tools),
+                "tool_names": lambda x: ", ".join([t.name for t in tools]),
+            }
+            | prompt
+            | llm
+            | ReActSingleInputOutputParser()
+        )
+        return llm_chain
