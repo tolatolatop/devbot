7a2f9aa7fbe54378df13e0f13b85e6e9a396f751
tolatolatop <tolatolatop@gmail.com>
2023-12-08 16:47:39 +0000

    临时删除接口

--- devbot/devbot.py
+++ devbot/devbot.py
@@ -3,7 +3,6 @@ import logging
 from fastapi import FastAPI, BackgroundTasks
 
 from devbot.repo import github
-from devbot.agent import core
 
 from devbot.controller import accept_event
 
@@ -19,14 +18,4 @@ async def root():
 
 @app.get("/healthz")
 async def healthz():
-    return {"message": "OK"}
-
-
-@app.post("/webhook/github")
-async def webhook_github(
-    event: github.IssueEvent, background_tasks: BackgroundTasks
-):
-    g = accept_event(event)
-    if g:
-        background_tasks.add_task(core.reslove_issue, event, g)
     return {"message": "OK"}
